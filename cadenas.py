9c74b5d32acbe5f0460a06c86ba1cfe22101360f
Gis Minero Nacional <43586731+gisminero@users.noreply.github.com>
2020-11-25 00:36:21 +0100

    Create cadenas.py

---
+++ cadenas.py
@@ -0,0 +1,27 @@
+#/!usr/bin/python
+#Carga de librerias
+import sys, subprocess
+#Definicion de la funcion principal, quien se ejecuta cuando comienza la ejecucion del script
+def main():
+    ######DEFINO VARIABLES A Utilizar
+    print '*******************************************************'
+    print '*******************************************************'
+    print 'Bienvenido'
+    #inicializo las variables a usar
+    lista_de_string = ['holA--Chau-Bye', 'CoMo AndaS?4124', 'SADRrfsqwfefrthWSA3123', '123123qadAdwqRErwewEd', 'ww12334SDDsSS13123e','fDDDDEDWdqdq8AS4DA8SDsdddS', 'qwdqwdqwdq2312312e21ce21g','412412412eqwease23e2h']
+    print '----------------------------------------'
+    print lista_de_string
+    '''
+    enunciado:
+    inicio de ejercicio
+    crear una branch para cada uno de los integrantes
+    tienen que crear una branch con cada uno de los nombre 
+    Ejercicio:
+    Dado un array con varios string se debe agregar una funciion que:
+    * cuente la cantidad de vocales por cada palabra (Dario)
+    * cuente la cantidad de mayusculas (Diego)
+    * cuente la suma de los valores ASCII de cada uno de los caracteres (Ale)
+    * Cantidad de ocurrencia de una letra en cada palabra. (Edu)
+    '''
+if __name__ == '__main__':  
+    main()
